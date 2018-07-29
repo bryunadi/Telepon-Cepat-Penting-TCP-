20717b997151ad64dc86b8ecf20a2a4a82179115
Bryan Yunadi <yunadibryan@gmail.com>
2018-07-30 00:52:35 +0700

    version 1.0

--- tcp/ViewController.swift
+++ tcp/ViewController.swift
@@ -18,8 +18,52 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    
     }
-
-
+  
+    @IBAction func rs(_ sender: Any) {
+        
+        let url:NSURL = NSURL (string: "tel://0811689110")!
+        UIApplication.shared.open(url as URL)
+        
+    }
+    
+    @IBAction func polisi(_ sender: Any) {
+        
+        let url:NSURL = NSURL (string: "tel://0811689110")!
+        UIApplication.shared.open(url as URL)
+        
+    }
+    
+    @IBAction func damkar(_ sender: Any) {
+        
+        let url:NSURL = NSURL (string: "tel://065144123")!
+        UIApplication.shared.open(url as URL)
+        
+    }
+    
+    @IBAction func pdam(_ sender: Any) {
+    
+        let url:NSURL = NSURL (string: "tel://081360537581")!
+        UIApplication.shared.open(url as URL)
+        
+    }
+    
+    @IBAction func dlhk(_ sender: Any) {
+        
+        let url:NSURL = NSURL (string: "tel://08116788444")!
+        UIApplication.shared.open(url as URL)
+        
+    }
+    
+    @IBAction func wh(_ sender: Any) {
+        
+        let url:NSURL = NSURL (string: "tel://081219314001")!
+        UIApplication.shared.open(url as URL)
+        
+    }
+    
+    
 }
+
 
